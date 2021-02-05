88b0ff5447101d7091f8074cfa89f508477c8701
DimaMint <dchepiga@gmail.com>
2021-02-05 17:06:54 +0600

    Изменения на ноуте, переносим на комп

---
+++ elements/js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// const box = document.getElementById('box');
+// console.log(box);
+
+// const buttons = document.getElementsByTagName('button');
+// console.log(buttons);
+
+// // document.getElementsByTagName('button')[0].innerText = "Button1-1";
+
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+
+// const hearts = document.querySelectorAll('.heart');
+
+// // hearts.forEach( item => {
+// //     console.log(item);
+// // })
+
+// const heart1 = document.querySelector('.heart');
+
+
+const   box = document.getElementById('box'),
+        buttons = document.getElementsByTagName('button'),
+        circles = document.getElementsByClassName('circle'),
+        hearts = document.querySelectorAll('.heart'),
+        heart1 = document.querySelector('.heart');
+
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
+
