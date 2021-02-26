a71bc6df35c0fb1a3f300985ba4e7a7cc0e47f54
Dima <dchepiga@gmail.com>
2021-02-26 21:30:12 +0600

    End Home

--- Modul3/lesson49/script.js
+++ Modul3/lesson49/script.js
@@ -1,9 +1,16 @@
 //rest operator
 "use strict";
 
-const log = function (a,b, ...rest) {
+const log = function (a,b, ...rest) { //? ... rest operator returns array
     console.log(a, b, rest);
     
+};
+
+// log(1, 2, 3, 5, 6, 7, 8);
+
+
+function calcOrDouble(number, basis = 2) { //? присвоение по умолчанию
+    console.log(number * basis);
 }
 
-log(1, 2, 3, 5, 6, 7, 8);
+calcOrDouble(2); 
