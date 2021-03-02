214c199f4fb49c0ceeedad7164e03f9b81c80e48
Dima Chepiga <dchepiga@gmail.com>
2021-03-02 16:24:20 +0600

    Work End

--- Modul3/lesson51/script.js
+++ Modul3/lesson51/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const persone = {
+    name: 'Alex',
+    tel: '+77777777',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
+};
+
+console.log(JSON.stringify(persone)); 
+//? Конвертация объекта в JSON формат
+
+console.log(JSON.parse(JSON.stringify(persone))); 
+//? Конвертация входящего JSON to Object
+
+
+const clone = JSON.parse(JSON.stringify(persone)); 
+//? Полная копия объекта. Без относительных ссылок
+
+clone.parents.mom = 'Nataly';
+
+console.log(persone);
+console.log(clone);
+
